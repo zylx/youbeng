4d905bd4e496838cf8306cf7ab0d2b98b9d6afb0
zylx <549535805@qq.com>
2020-07-04 10:06:03 +0800

    修改postcss.config.js配置文件的autoprefixer插件browers属性为overrideBrowserslist

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,10 @@ module.exports = {
     plugins: [
         require('autoprefixer')({
             //必须设置支持的浏览器才会自动添加添加浏览器兼容
-            browsers: [
+            overrideBrowserslist: [
+                "> 1%",
                 "last 2 versions",
-                "> 1%"
+                "not ie <= 8"
             ]
         })
     ]
